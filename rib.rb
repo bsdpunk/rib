36b50ef9a6af877c51bc1ffcd59dd37ed3ed7358
Dustin Carver <bsdpunk@gmail.com>
2019-02-26 12:51:45 -0600

    CleanUp

--- rib.rb
+++ rib.rb
@@ -2,17 +2,13 @@ require 'Resolv'
 require "ipaddress"
 
 res = Resolv::DNS.new(:nameserver => [ARGV[1]],
-                :search => [''],
-                :ndots => 1)
-#res.each do |key, value|
-  #puts key + ' : ' + value
-#end
-#puts res.getaddress(ARGV[0])
-typeclass = Resolv::DNS::Resource::IN::PTR
-  if  IPAddress.valid? ARGV[0]
+                      :search => [''],
+                      :ndots => 1)
+
+if  IPAddress.valid? ARGV[0]
 
   puts res.getname(ARGV[0])
-  else
+else
   puts res.getaddress(ARGV[0])
 end
 
